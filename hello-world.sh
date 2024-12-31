d9964989399e8afa5dcea7ac2819b8b2fd3b5594
Bode F <bode.falade@outlook.com>
2024-12-30 23:15:09 -0600

    added hello-world script.

---
+++ hello-world.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Running Hello World from Python 2:"
+python2 -c "print('Hello World from Python 2')"
+
+echo "Running Hello World from Python 3:"
+python3 -c "print('Hello World from Python 3')"
+
+echo "Running Hello World from R:"
+Rscript -e "cat('Hello World from R\n')"
