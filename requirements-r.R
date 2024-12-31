c571081fe25831e42a26d5d364de0744497d261e
Bode F <bode.falade@outlook.com>
2024-12-30 22:49:55 -0600

    added R dependencies

---
+++ requirements-r.R
@@ -0,0 +1,10 @@
+# R requirements file
+
+install.packages("ggplot2", repos = "http://cran.us.r-project.org")    # for data visualization
+install.packages("dplyr", repos = "http://cran.us.r-project.org")      # for data manipulation
+install.packages("tidyr", repos = "http://cran.us.r-project.org")      # for data tidying
+install.packages("readr", repos = "http://cran.us.r-project.org")      # Reading data files req
+install.packages("caret", repos = "http://cran.us.r-project.org")      # Machine learning req
+install.packages("shiny", repos = "http://cran.us.r-project.org")      # Web apps req
+install.packages("plotly", repos = "http://cran.us.r-project.org")     # Interactive plots req
+install.packages("stringr", repos = "http://cran.us.r-project.org")    # String manipulation req
